0be80ff892abc451f8f4161015ff3109addbc7e0
Borges - 746 <borges@ciclocairu.com.br>
2023-05-16 07:04:22 -0400

    criacao do header da home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { StatusBar } from "react-native";
+
+import Logo from "../../assets/logo.svg";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 export function Home() {
   return (
     <Container>
-      <Title>Teste</Title>
+      <HeaderContent>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Header>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </Header>
+      </HeaderContent>
     </Container>
   );
 }
