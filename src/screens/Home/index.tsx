46109bda68ef7cb9cddffb78677b1acbe7383759
Borges - 746 <borges@ciclocairu.com.br>
2023-05-18 07:14:46 -0400

    cartao do carro

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,20 +6,36 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
+import { Car } from "../../components/Car";
+
 export function Home() {
+  const carDataOne = {
+    brand: "Audi",
+    name: "Rs 5 Coupe",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+
+    thumbnail:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fbaixar%2Faudi-cars.html&psig=AOvVaw3BLdl9LKgpkcpBivwvpPgq&ust=1684494111707000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNicotrb_v4CFQAAAAAdAAAAABAJ",
+  };
+
   return (
     <Container>
-      <HeaderContent>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <Header>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
           <TotalCars>Total de 12 carros</TotalCars>
-        </Header>
-      </HeaderContent>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carDataOne} />
     </Container>
   );
 }
