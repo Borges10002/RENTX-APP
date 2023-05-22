9922c71e6c26c2c9902da73701f7483c40047b2e
Borges - 746 <borges@ciclocairu.com.br>
2023-05-22 07:20:10 -0400

    componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Container, Header } from "./styles";
+
+import { Container, Header, CarImages } from "./styles";
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
 export function CarDetails() {
   return (
@@ -8,6 +10,14 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fbaixar%2Faudi-cars.html&psig=AOvVaw3BLdl9LKgpkcpBivwvpPgq&ust=1684494111707000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNicotrb_v4CFQAAAAAdAAAAABAJ",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
