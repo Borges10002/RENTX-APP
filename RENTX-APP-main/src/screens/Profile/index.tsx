8269cc4273627b286c659c4fbcb40051552c0b52
Borges10002 <borges10002@gmail.com>
2023-08-25 21:34:16 -0400

    update

--- RENTX-APP-main/src/screens/Profile/index.tsx
+++ RENTX-APP-main/src/screens/Profile/index.tsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import { useTheme } from "styled-components";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 
 import {
   Container,
@@ -17,14 +23,19 @@ import {
   Options,
   Option,
   OptionTitle,
+  Section,
 } from "./styles";
 import { BackButton } from "../../components/BackButton";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
+import { useAuth } from "../../hooks/auth";
 
 export function Profile() {
   const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
 
   const theme = useTheme();
   const navigation = useNavigation();
+  const { user } = useAuth();
 
   function handleBack() {
     navigation.goBack();
@@ -37,45 +48,77 @@ export function Profile() {
   }
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton color={theme.colors.shape} onPress={handleBack} />
-          <HeaderTitle>Editar Perfil</HeaderTitle>
-          <LogoutButton onPress={handleSinOut}>
-            <Feather name="power" size={24} color={theme.colors.shape} />
-          </LogoutButton>
-        </HeaderTop>
-        <PhotoContainer>
-          <Photo
-            source={{
-              uri: "https://avatars.githubusercontent.com/u/32522338?v=4",
-            }}
-          />
-          <PhotoButton onPress={() => {}}>
-            <Feather name="camera" size={24} color={theme.colors.shape} />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback>
+        <Container>
+          <Header>
+            <HeaderTop>
+              <BackButton color={theme.colors.shape} onPress={handleBack} />
+              <HeaderTitle>Editar Perfil</HeaderTitle>
+              <LogoutButton onPress={handleSinOut}>
+                <Feather name="power" size={24} color={theme.colors.shape} />
+              </LogoutButton>
+            </HeaderTop>
+            <PhotoContainer>
+              <Photo
+                source={{
+                  uri: "https://avatars.githubusercontent.com/u/32522338?v=4",
+                }}
+              />
+              <PhotoButton onPress={() => {}}>
+                <Feather name="camera" size={24} color={theme.colors.shape} />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
 
-      <Content>
-        <Options>
-          <Option
-            active={option === "dataEdit"}
-            onPress={() => handleOptionChange("dataEdit")}
-          >
-            <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
-          </Option>
-          <Option
-            active={option === "passwordEdit"}
-            onPress={() => handleOptionChange("passwordEdit")}
-          >
-            <OptionTitle active={option === "passwordEdit"}>
-              Trocar senha
-            </OptionTitle>
-          </Option>
-        </Options>
-      </Content>
-    </Container>
+          <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+            <Options>
+              <Option
+                active={option === "dataEdit"}
+                onPress={() => handleOptionChange("dataEdit")}
+              >
+                <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
+              </Option>
+              <Option
+                active={option === "passwordEdit"}
+                onPress={() => handleOptionChange("passwordEdit")}
+              >
+                <OptionTitle active={option === "passwordEdit"}>
+                  Trocar senha
+                </OptionTitle>
+              </Option>
+            </Options>
+
+            {option === "dataEdit" ? (
+              <Section>
+                <Input
+                  iconName="user"
+                  placeholder="Nome"
+                  autoCorrect={false}
+                  defaultValue={user.name}
+                />
+                <Input
+                  iconName="mail"
+                  editable={false}
+                  defaultValue={user.email}
+                />
+                <Input
+                  iconName="credit-card"
+                  placeholder="CNH"
+                  keyboardType="numeric"
+                  defaultValue={user.driver_license}
+                />
+              </Section>
+            ) : (
+              <Section>
+                <PasswordInput iconName="lock" placeholder="Senha atual" />
+                <PasswordInput iconName="lock" placeholder="Nova senha" />
+                <PasswordInput iconName="lock" placeholder="Repetir Senha" />
+              </Section>
+            )}
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- RENTX-APP-main/src/screens/Profile/styles.ts
+++ RENTX-APP-main/src/screens/Profile/styles.ts
@@ -9,7 +9,6 @@ interface OptionProps {
 }
 
 export const Container = styled.View`
-  flex: 1;
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
@@ -70,7 +69,6 @@ export const PhotoButton = styled(RectButton)`
 `;
 
 export const Content = styled.View`
-  flex: 1;
   padding: 0 24px;
   margin-top: 122px;
 `;
@@ -103,3 +101,5 @@ export const OptionTitle = styled.Text<OptionProps>`
   color: ${({ theme, active }) =>
     active ? theme.colors.header : theme.colors.text_detail};
 `;
+
+export const Section = styled.View``;
